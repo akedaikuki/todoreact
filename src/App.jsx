46b4c42b6ee43371fd90529d06167f58b1b323b8
ikuki <akedaikuki@gmail.com>
2023-07-26 11:11:24 +0800

    feat: write deploy scripts

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import { AuthProvider } from 'contexts/AuthContext';
 // 讓 <AuthProvider> 包住所有元件，如此一來子元件就可以取用 Context 的共享內容
 // 但需要放在 <BrowserRouter> 裡面，因為我們在 AuthProvider 裡有使用到瀏覽器的資訊
 
+const basename = process.env.PUBLIC_URL;
+
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <AuthProvider>
           <Routes>
             <Route path="login" element={<LoginPage />} />
